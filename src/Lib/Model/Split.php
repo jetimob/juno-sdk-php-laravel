4edc64f75743b72c245f474004481e014cc5d919
hnvas <henriquenvasconcelos@gmail.com>
2020-10-07 23:30:58 -0300

    fix: split amount type

--- src/Lib/Model/Split.php
+++ src/Lib/Model/Split.php
@@ -11,8 +11,8 @@ class Split
     /** @var string|null $recipientToken defines the recipient of the split */
     public ?string $recipientToken;
 
-    /** @var int|null $amount defines the fixed value that the account will receive */
-    public ?int $amount;
+    /** @var float|null $amount defines the fixed value that the account will receive */
+    public ?float $amount;
 
     /** @var float|null $percentage defines the percentage of the value that the account will receive */
     public ?float $percentage;
