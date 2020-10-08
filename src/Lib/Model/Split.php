6cad32ae1d8c9e74993c883f917b67706e9270a9
hnvas <henriquenvasconcelos@gmail.com>
2020-10-07 23:02:05 -0300

    fix: inaccessible split model attributes

--- src/Lib/Model/Split.php
+++ src/Lib/Model/Split.php
@@ -9,19 +9,19 @@ class Split
     use Serializable;
 
     /** @var string|null $recipientToken defines the recipient of the split */
-    private ?string $recipientToken;
+    public ?string $recipientToken;
 
     /** @var int|null $amount defines the fixed value that the account will receive */
-    private ?int $amount;
+    public ?int $amount;
 
     /** @var float|null $percentage defines the percentage of the value that the account will receive */
-    private ?float $percentage;
+    public ?float $percentage;
 
     /** @var bool|null $amountReminder indicates who receives the remaining amount in the case of a division of the
      * total transaction amount */
-    private ?bool $amountReminder;
+    public ?bool $amountReminder;
 
     /** @var bool|null $chargeFee indicates whether only one recipient will be taxed or if the fees will be split
      * proportionally between all recipients */
-    private ?bool $chargeFee;
+    public ?bool $chargeFee;
 }
