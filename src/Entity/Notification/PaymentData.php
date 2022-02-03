9482b3eb783e34e2093442edf1361906edc795cc
medina <ammedina@inf.ufsm.br>
2022-02-03 10:26:49 -0300

    feat: adiciona constante para o novo evento de pagamento `PRECONFIRMED`

--- src/Entity/Notification/PaymentData.php
+++ src/Entity/Notification/PaymentData.php
@@ -9,6 +9,15 @@ class PaymentData
     use Serializable;
     use EntityBaseTrait;
 
+    public const BANK_PAID_BACK = 'BANK_PAID_BACK';
+    public const PRE_CONFIRMED = 'PRECONFIRMED';
+    public const CONFIRMED = 'CONFIRMED';
+    public const PARTIALLY_REFUNDED = 'PARTIALLY_REFUNDED';
+    public const CUSTOMER_PAID_BACK = 'CUSTOMER_PAID_BACK';
+    public const DECLINED = 'DECLINED';
+    public const FAILED = 'FAILED';
+    public const NOT_AUTHORIZED = 'NOT_AUTHORIZED';
+
     protected PaymentAttributes $attributes;
 
     /**
